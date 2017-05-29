55d459d1741fb259cee48fd978aec8cb5dc55da8
TankHeroPro <andreas.schranz22@gmail.com>
2017-05-29 14:31:43 +0200

    created javaproject
    created the base form of the javaproject

---
+++ java/calendarproject/src/calendar/Controller.java
@@ -0,0 +1,4 @@
+package calendar;
+
+public class Controller {
+}
---
+++ java/calendarproject/src/calendar/Main.java
@@ -0,0 +1,23 @@
+package calendar;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("Mainwindow.fxml"));
+        primaryStage.setTitle("Calendar");
+        primaryStage.setScene(new Scene(root, 600, 600));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
