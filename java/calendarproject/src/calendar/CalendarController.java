f2e617d8b8c8059dc2383a222a8096f54c8ef011
TankHeroPro <andreas.schranz22@gmail.com>
2017-06-19 14:12:37 +0200

    Deleted code
    Deleted useless code

--- java/calendarproject/src/calendar/CalendarController.java
+++ java/calendarproject/src/calendar/CalendarController.java
@@ -19,7 +19,7 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class CalendarController {
-	
+
 	@FXML
 	private ListView<Calendar> myListView;
 	private ObservableList<Calendar> listViewData = FXCollections.observableArrayList();
@@ -29,49 +29,32 @@ public class CalendarController {
     public void setPrevStage(Stage stage){
          this.prevStage = stage;
     }
-    
+
     public void setMain(Main main){
         this.main = main;
    }
 
 	public CalendarController() throws IOException {
-		listViewData.add(new Calendar("Essen am Samstag"));
-		listViewData.add(new Calendar("Hausaufgaben"));
 	}
-		
+
 	@FXML
     private void handleDeleteNotiz(ActionEvent event) throws IOException {
-       	Calendar selectedCalendar = myListView.getSelectionModel().getSelectedItem();
-        if (selectedCalendar != null) {
-        	
-        	listViewData.remove(selectedCalendar);
-        	myListView.refresh();
 
-		} else {
-	        // Nothing selected.
-	        Alert alert = new Alert(AlertType.WARNING);
-	        alert.initOwner(prevStage);
-	        alert.setTitle("No Selection");
-	        alert.setHeaderText("No note selected");
-	        alert.setContentText("Select a note first");
-	
-	        alert.showAndWait();
-	    }
     }
-	
+
 	@FXML
     private void handleEditNotiz(ActionEvent event) throws IOException {
         Calendar selectedCalendar = myListView.getSelectionModel().getSelectedItem();
         if (selectedCalendar != null) {
-        	
-        	Stage stage; 
+
+        	Stage stage;
         	AnchorPane root;
-        	
+
        		FXMLLoader myLoader = new FXMLLoader(main.getClass().getResource("EditView.fxml"));
     		root = (AnchorPane) myLoader.load();
     		CalendarEditController controller = (CalendarEditController) myLoader.getController();
     		controller.setCalendar(selectedCalendar);
-			 
+
         	stage = new Stage();
         	stage.setScene(new Scene(root));
         	stage.setTitle("Edit Note Dialog");
@@ -80,7 +63,7 @@ public class CalendarController {
         	stage.initOwner(prevStage);
         	controller.setPrevStage(stage);
         	stage.showAndWait();
-        	
+
         	myListView.refresh();
 
 		} else {
@@ -90,95 +73,28 @@ public class CalendarController {
 	        alert.setTitle("No Selection");
 	        alert.setHeaderText("No note selected");
 	        alert.setContentText("Select a note first");
-	
+
 	        alert.showAndWait();
 	    }
     }
-	
+
 	@FXML
     private void handleNewNote(ActionEvent event) throws IOException {
-        //Calendar selectedNotiz = myListView.getSelectionModel().getSelectedItem();
-		Calendar newNote = new Calendar();
-		listViewData.add(newNote);
-          	
-    	Stage stage; 
-    	AnchorPane root;
-    	
-   		FXMLLoader myLoader = new FXMLLoader(main.getClass().getResource("EditView.fxml"));
-		root = (AnchorPane) myLoader.load();
-		CalendarEditController controller = (CalendarEditController) myLoader.getController();
-		controller.setCalendar(newNote);
-		 
-    	stage = new Stage();
-    	stage.setScene(new Scene(root));
-    	stage.setTitle("New Note Dialog");
-    	stage.initModality(Modality.APPLICATION_MODAL);
 
-    	stage.initOwner(prevStage);
-    	controller.setPrevStage(stage);
-    	stage.showAndWait();
-    	
-    	myListView.refresh();
     }
-	
-		
-	@FXML
-	private void initialize() {	
-				
-		// Init ListView.
-		myListView.setItems(listViewData);
 
-		myListView.setCellFactory((list) -> {
-			return new ListCell<Calendar>() {
-				@Override
-				protected void updateItem(Calendar item, boolean empty) {
-					super.updateItem(item, empty);
-					
-					if (item == null || empty) {
-						setText(null);
-					} else {
-						setText(item.getDate() + "  :  " + item.getText());
-					}
-				}
-			};
-		});
-		
-		/* Handle ListView selection changes.
-		myListView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-			//outputTextArea.appendText("ListView Selection Changed (selected: " + newValue.toString() + ")\n");
-		});	*/	
-		
-		
-		// Maus Doppel Click soll editieren
-		myListView.setOnMouseClicked(new EventHandler<MouseEvent>() {
-		    @Override
-		    public void handle(MouseEvent click) {
-		    	Calendar selectedCalendar;
-		        if (click.getClickCount() == 2) {
-		        	selectedCalendar = myListView.getSelectionModel().getSelectedItem();
-			        if (selectedCalendar != null) {
-			        	try {
-							handleEditNotiz(null);
-						} catch (IOException e) {
-							e.printStackTrace();
-						}	
-			        }
-		        }
-		        else {
-		        	myListView.edit(myListView.getSelectionModel().getSelectedIndex());
-		        	
-		        }
-		    }
-		});	
+
+	@FXML
+	private void initialize() {
 
 	}
-	
-	
+
+
 	@FXML
     private void handleClose(ActionEvent event) throws IOException {
 		Platform.exit();
     }
-	
+
 	@FXML
     private void handleAbout(ActionEvent event) throws IOException {
 		Alert alert = new Alert(AlertType.INFORMATION);
@@ -188,5 +104,5 @@ public class CalendarController {
         alert.setContentText("2017 by Simon Kunz, Andreas Schranz");
         alert.showAndWait();
     }
-	
+
 }
