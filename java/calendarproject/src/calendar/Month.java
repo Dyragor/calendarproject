de3b0e8e092c40fc48af6cde60893d62d3b94157
TankHeroPro <andreas.schranz22@gmail.com>
2017-06-26 15:10:08 +0200

    Bug fixes

--- java/calendarproject/src/calendar/Month.java
+++ java/calendarproject/src/calendar/Month.java
@@ -4,5 +4,35 @@ package calendar;
  * Created by 5ia15anschranz on 26.06.2017.
  */
 public enum Month {
-    JANUARY, FEBRUARY, MARS, APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER
+    JANUARY, FEBRUARY, MARS, APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER;
+    public static int faerbung(Month month){
+        switch (month){
+            case JANUARY:
+                return 31;
+            case FEBRUARY:
+                return 28;
+            case MARS:
+                return 31;
+            case APRIL:
+                return 30;
+            case MAY:
+                return 31;
+            case JUNE:
+                return 30;
+            case JULY:
+                return 31;
+            case AUGUST:
+                return 31;
+            case SEPTEMBER:
+                return 30;
+            case OCTOBER:
+                return 31;
+            case NOVEMBER:
+                return 30;
+            case DECEMBER:
+                return 31;
+            default:
+                return 0;
+        }
+    }
 }
